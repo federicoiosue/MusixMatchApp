ce6e7c344635fd75c3d227eff0c8a7c1ccfa3709
Federico Iosue <federico@iosue.it>
2017-11-24 01:38:07 +0100

    Moved API_KEY and API_BASE_URL into module's unversioned properties to reduce information disclosure

--- app/src/main/java/android/feio/it/musixmatchapp/services/ServicesHelper.java
+++ app/src/main/java/android/feio/it/musixmatchapp/services/ServicesHelper.java
@@ -1,5 +1,6 @@
 package android.feio.it.musixmatchapp.services;
 
+import android.feio.it.musixmatchapp.BuildConfig;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import io.reactivex.Flowable;
 import retrofit2.Retrofit;
@@ -8,17 +9,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServicesHelper {
 
-	private static final String API_BASE_URL = "http://api.musixmatch.com/ws/1.1/";
-
 	private static final String TAG = ServicesHelper.class.getSimpleName();
 
 	public static Flowable<Object> getTracks() {
-		Retrofit asd = new Retrofit.Builder()
-				.baseUrl(API_BASE_URL)
+		return getServices().getTracksChart("it");
+	}
+
+	private static ServicesInterface getServices() {
+		return new Retrofit.Builder()
+				.baseUrl(BuildConfig.API_BASE_URL)
 				.addConverterFactory(GsonConverterFactory.create())
 				.addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-				.build();
-		return asd.create(ServicesInterface.class).getTracksChart("it");
+				.build()
+				.create(ServicesInterface.class);
 	}
 
 }
--- app/src/main/java/android/feio/it/musixmatchapp/services/ServicesInterface.java
+++ app/src/main/java/android/feio/it/musixmatchapp/services/ServicesInterface.java
@@ -1,5 +1,6 @@
 package android.feio.it.musixmatchapp.services;
 
+import android.feio.it.musixmatchapp.BuildConfig;
 import io.reactivex.Flowable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -7,8 +8,6 @@ import retrofit2.http.Query;
 
 public interface ServicesInterface {
 
-	String APIKEY = "asd";
-
-	@GET("chart.tracks.get?f_has_lyrics=1&apikey=" + APIKEY)
+	@GET("chart.tracks.get?f_has_lyrics=1&apikey=" + BuildConfig.API_KEY)
 	Flowable<Object> getTracksChart(@Query("country") String country);
 }
