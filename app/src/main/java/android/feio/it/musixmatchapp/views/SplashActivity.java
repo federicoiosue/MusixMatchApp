c78a15c5cf74ed8db6226054fbf23c9a8cdd77ef
Federico Iosue <federico.iosue@gmail.com>
2017-11-27 19:33:31 +0100

    Added app icon, splash screen and changed colors style

---
+++ app/src/main/java/android/feio/it/musixmatchapp/views/SplashActivity.java
@@ -0,0 +1,17 @@
+package android.feio.it.musixmatchapp.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, TrackListActivity.class));
+        finish();
+    }
+
+}
